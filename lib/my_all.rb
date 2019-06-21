5c157c97578796bf8308ed41dc7855f4b14cf4b8
Adam Ervin <adamjervin@gmail.com>
2019-06-21 19:06:47 +0000

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,19 @@
-require 'pry'
-
 def my_all?(collection)
+i = 0 
+panth = []
+while i < collection.length 
+panth << 
+yield(collection[i])
+i = i + 1 
 
+end
+
+
+
+
+ if panth.include?(false)
+    false
+  else
+    true
+  end
 end
